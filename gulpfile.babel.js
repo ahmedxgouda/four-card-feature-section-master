5b5069eba047b33d5212247d8b0f180d2c7307de
Ahmed Goda <ahmedmosad405@gmail.com>
2020-04-21 19:03:36 +0200

    Adding ES6 to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,10 @@
 // jshint esversion: 6
-const { src, dest, watch, series } = require('gulp');
-var sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+import { src, dest, watch, series } from 'gulp';
+import sass, { logError } from 'gulp-sass';
+import { init } from 'browser-sync';
 function css() {
       return src('./css/*.scss')
-      .pipe(sass().on('error', sass.logError))
+      .pipe(sass().on('error', logError))
       .pipe(dest('./css'));      
 }
 function sass_watch(done) {
@@ -17,13 +17,16 @@ function browser_sync(done) {
             './css/*.css',
             './images/*.*'
       ];
-      browserSync.init(files, {
+      init(files, {
             server: {
                   baseDir: './'
             }
       });
       done();
 }
-exports.sass = css;
-exports.sass_watch = sass_watch;
-exports.default = series(browser_sync, sass_watch);
+const _sass = css;
+export { _sass as sass };
+const _sass_watch = sass_watch;
+export { _sass_watch as sass_watch };
+const _default = series(browser_sync, sass_watch);
+export { _default as default };
